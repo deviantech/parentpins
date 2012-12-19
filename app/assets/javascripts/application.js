e2cb464bf4de5bc7ba3f311d92b8eedb40157be9
Kali Donovan <kali@deviantech.com>
2012-12-18 16:27:18 -0800

    Converting remaining static files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,22 @@
-// Actual application code goes here
+$(document).ready(function() {
+  $('.pinly_toggle').click(function(){
+	//get collapse content selector
+	var collapse_content_selector = $(this).attr('href');					
+
+	//make the collapse content to be shown or hide
+	var toggle_switch = $(this);
+	$(collapse_content_selector).toggle(function(){
+	  if($(this).css('display')=='none'){
+                            //change the button label to be 'Show'
+		toggle_switch.html('Show');
+	  }else{
+                            //change the button label to be 'Hide'
+		toggle_switch.html('Hide');
+	  }
+	});
+  });
+
+});	
 
 $(document).ready(function() {
   $('#pins').masonry({
@@ -8,3 +26,4 @@ $(document).ready(function() {
    $('#pins').masonry('reload');
   });
 });
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  # TODO -- this will be replaced when we wire up actual users
+  def logged_in?
+    false
+  end
   
   def nav_link(text, target, opts = {})
     opts[:class] ||= ''
@@ -7,8 +12,13 @@ module ApplicationHelper
   end
   
   def body_class
-    if params[:action] == 'recent_pins'
+    case params[:action]
+    when 'recent_pins', 'articles', 'board', 'board_landing'
       'full_width'
+    when 'about', 'login', 'signup'
+      'theme_one'
+    when 'modal_edit_pin', 'modal_signup_2'
+      'modal'
     else
       ''
     end
