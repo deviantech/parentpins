554a93f8957c7fa917ba47cdc732eeae5a6dd191
kyle kothe <kylekothe@kyle-kothes-macbook-pro.local>
2012-12-19 23:12:27 -0600

    finalized pin modal overlay

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Kidpins::Application.config.session_store :cookie_store, key: '_kidpins_session'
+Kidpins::Application.config.session_store :cookie_store, :key => '_kidpins_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
