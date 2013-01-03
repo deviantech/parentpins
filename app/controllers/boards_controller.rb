9bee30f1125247e6ca8b676e581dedd3bf30401c
Mike Cravey <techie@craveytrain.com>
2013-01-02 21:33:14 -0600

    Fixes controller typo

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,16 +7,16 @@ class BoardsController < ApplicationController
     @category = Category.find_by_id(params[:category]) unless params[:category].blank?
     @boards = Board.in_category(@category).limit(20)
   end
-  
+
   def show
     @board = Board.find(params[:id])
     @pins = @board.pins.limit(20)
   end
-  
+
   def new
     @board = current_user.boards.new(params[:board])
   end
-  
+
   def create
     @board = current_user.boards.new(params[:board])
     if @board.save
@@ -26,11 +26,11 @@ class BoardsController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     render :action => 'new'
   end
-  
+
   def update
     if @board.update_attributes(params[:board])
       redirect_to @board, :notice => 'Saved changes to board'
@@ -39,16 +39,16 @@ class BoardsController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def destroy
     @board.destroy
-    redirect_to boards_profile_path(current_user, :notice => 'Removed Board'
+    redirect_to boards_profile_path(current_user, :notice => 'Removed Board')
   end
-  
+
   protected
-  
+
   def find_board
     @board = current_user.boards.find(params[:id])
   end
-  
+
 end
