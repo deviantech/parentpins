2846a4558f29ff7e2d2f687121acc2fa13c15eee
Kali Donovan <kali@deviantech.com>
2013-01-26 14:52:34 -0800

    Another asset to be compiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ ParentPins::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( popup.js popup.css bookmarklet.js )
+  config.assets.precompile += %w( popup.js popup.css bookmarklet.js bookmarklet.css )
 
   
   # Disable delivery errors, bad email addresses will be ignored
