9bd6acfad7851f530cff09d0488adfa4e8375eba
Kali Donovan <kali@deviantech.com>
2013-01-26 17:44:06 -0800

    Default to activity page

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -7,7 +7,7 @@ module ProfilesHelper
     string = string[0, string.length-1] if action != :following && string[-1] == 's' && @profile_counters[action].to_i == 1
     
     txt = content_tag(:span, @profile_counters[action], :class => 'counter') + ' ' + content_tag(:span, string, :class => 'label')
-    url = self.send("#{action}_profile_path", @profile)
+    url = action == :boards ? profile_boards_path(@profile) : self.send("#{action}_profile_path", @profile)
     classes = [action, current_page?(url) ? 'active' : nil].compact.join(' ')
     
     link_to(txt.html_safe, url, :class => classes)
