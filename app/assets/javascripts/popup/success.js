7e898117976f7b12c0a892e0799e4d7fc93327c9
Kali Donovan <kali@deviantech.com>
2013-02-08 14:34:50 -0800

    Added 'close' button to popup success page

--- app/assets/javascripts/popup/success.js
+++ app/assets/javascripts/popup/success.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     // Open link in opening window, and close the popup
     if (window.opener) {
       $(document).on('click', 'a', function(e) {
+        if ($(this).hasClas('js-ignore')) return true;
         e.preventDefault();
         window.opener.location = $(this).attr('href');
         window.close();
