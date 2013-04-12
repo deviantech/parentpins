0da1186ee5f8b5fbeb9c7cacb52aa7deb1e95a6d
kyle kothe <kylekothe@kyle-kothes-macbook-pro.local>
2013-04-11 22:06:01 -0500

    updated flash message styling

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -43,8 +42,6 @@ ActiveRecord::Schema.define(:version => 20130411230609) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  add_index "categories", ["name"], :name => "index_categories_on_name", :unique => true
 
   create_table "comments", :force => true do |t|
     t.text     "body"
