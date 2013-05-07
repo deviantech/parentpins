5c410e8c14c2c0b218451efc601998f2bb2cfd69
Kali Donovan <kali@deviantech.com>
2013-05-06 17:43:27 -0700

    WIP - adding shim for moving to step 2

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -82,7 +82,7 @@ class BoardController < ApplicationController
   def mine
     respond_to do |format|
       format.json {
-        render :json => (user_signed_in? ? current_user.boards : []).to_json(:only => [:name, :pins_count, :cover], :methods => [:followers_even_indirectly_count]), :callback => params[:callback]
+        render :json => (user_signed_in? ? current_user.boards : []).to_json(:only => [:id, :name, :pins_count, :cover], :methods => [:followers_even_indirectly_count]), :callback => params[:callback]
       }
     end
   end
