c01db7b7125f936dbbae41f57d1dee7859b961bf
Kali Donovan <kali@deviantech.com>
2013-07-12 19:55:54 -0700

    twitter tweak

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,7 +52,7 @@ module ApplicationHelper
       tags << meta_tag( 'og:type',        'website')      
       tags << meta_tag( 'twitter:card',   'gallery')
       @board.pins.with_image.limit(4).each_with_index do |pin, idx|
-        tags << meta_tag( "twitter:card:image#{idx}:src",   absolute_url(pin.image.v222.url))
+        tags << meta_tag( "twitter:image#{idx}:src",   absolute_url(pin.image.v222.url))
       end
       
       [title, desc, url, img]
