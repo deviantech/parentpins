a0f91a21334a5ef079c06059849d4882895fbf3b
Kali Donovan <kali@deviantech.com>
2013-07-26 12:40:52 -0700

    Testing pagination

--- db/migrate/20130725014141_keep_digest_of_original_image_source_url.rb
+++ db/migrate/20130725014141_keep_digest_of_original_image_source_url.rb
@@ -1,6 +1,14 @@
 class KeepDigestOfOriginalImageSourceUrl < ActiveRecord::Migration
   def up
     add_column :pins, :source_url, :string
+    Pin.reset_column_information
+    Pin.find_each do |p|
+      next unless p.source_url.blank?
+      
+      src = File.basename(p.image.path.to_s).to_s
+      src = src.length > 1 ? src : "pin-#{p.id}-source"
+      p.update_attribute :source_url, "OLD:#{src}"
+    end
   end
 
   def down
