933d1ab687076f0d462aa657326fcd8637b93d6a
Kali Donovan <kali@deviantech.com>
2013-10-09 11:24:47 -0700

    Importer step 2/3 improvements

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -7,5 +7,13 @@ module ImportHelper
     end
   end
   
+  # Render as a link, if it's before this step, and just the block content otherwise
+  def previous_link(step, n, &block)
+    if n < step
+      link_to(self.send("pin_import_step_#{n}_path"), :class => 'previous', &block)
+    else
+      content_tag(:span, &block)
+    end
+  end
   
 end
