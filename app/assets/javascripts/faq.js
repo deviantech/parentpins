2d2fa4105a18394a7f30ba9553d1377bc7fb8ad9
Kali Donovan <kali@deviantech.com>
2013-11-04 12:44:31 -0800

    Adding affix back via JS

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,1 +1,17 @@
 //= require contrib/highlight
+
+hljs.initHighlightingOnLoad();
+
+var wrapper = $('#faq_nav_wrapper');
+var spybar = $('ul.spy_navbar');
+
+// Keep ul as narrow as the wrapper that would be containing it without the affix-ness
+function rebindWidth() {
+  spybar.width( wrapper.width() );
+}
+
+if ($(window).width() >= 770){	
+  rebindWidth();
+	spybar.affix();
+  $(window).resize(rebindWidth);
+}
