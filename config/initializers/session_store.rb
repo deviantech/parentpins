096eacd7a3ff83951b751662343ff903a53ab7c5
Kali Donovan <kali@deviantech.com>
2013-11-26 12:04:29 -0800

    Explicitly sharing cookies across subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-ParentPins::Application.config.session_store :cookie_store, :key => '_pins_session'
+domain = case Rails.env
+when 'production' then '.parentpins.com'
+when 'staging'    then 'staging.parentpins.com'
+else :all
+end
+
+ParentPins::Application.config.session_store :cookie_store, :key => '_pins_session', :domain => domain
