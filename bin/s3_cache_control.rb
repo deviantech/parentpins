903a1b9bb966c236bbeab768642a99707738c9f6
Kali Donovan <kali@deviantech.com>
2013-11-26 13:50:04 -0800

    Tweaks to method

--- bin/s3_cache_control.rb
+++ bin/s3_cache_control.rb
@@ -4,7 +4,9 @@ require 'aws-sdk'
 
 bucket_name = ARGV[0]
 
-puts "Beginning to update #{bucket_name}"
+started = Time.now
+puts "Beginning to update #{bucket_name} at #{started}"
+
 
 s3 = AWS.s3
 bucket = s3.buckets[bucket_name]
@@ -17,9 +19,12 @@ bucket.objects.each do |obj|
   obj.copy_to(obj.key, :acl => :public_read, :cache_control => 'public, max-age=315576000', :content_type => obj.content_type)
 end
 
+puts "Completed run in #{Time.now - started} seconds"
 
 __END__
 
 # EXPORT AWS KEYS
 source ~/.ec2/parentpins/set-aws
-ruby bin/s3_cache_control.rb parentpins-staging
+ruby bin/s3_cache_control.rb parentpins
+
+# started run at 1:23
