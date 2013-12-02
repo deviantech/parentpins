6016cf688db1f7a25c69702140e07008e3e97154
Kali Donovan <kali@deviantech.com>
2013-12-02 11:01:50 -0800

    Adding bitty bit of caching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :commentable_id, :commentable_type, :user_id
   
   belongs_to :user
-  belongs_to :commentable,  :polymorphic => true, :counter_cache => true
+  belongs_to :commentable,  :polymorphic => true, :counter_cache => true, :touch => true
   
   validates_presence_of :commentable, :user
   validates_length_of :body, :minimum => 1
