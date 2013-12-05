caba6dd65ecd8dcad029494a32a4f693efc10f27
Kali Donovan <kali@deviantech.com>
2013-12-05 13:11:44 -0800

    Caching age groups and categories

--- app/models/age_group.rb
+++ app/models/age_group.rb
@@ -3,4 +3,11 @@ class AgeGroup < ActiveRecord::Base
   attr_accessible :name
   
   validates_uniqueness_of :name, :allow_blank => false
+  
+  def self.all_cached
+    Rails.cache.fetch('all_age_groups', :expires_in => 1.hour) do
+      AgeGroup.all
+    end
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,10 @@ class Category < ActiveRecord::Base
   validates_uniqueness_of :name, :allow_blank => false  
   
   default_scope { order('name ASC') }
+  
+  def self.all_cached
+    Rails.cache.fetch('all_categories', :expires_in => 1.hour) do
+      Category.all
+    end
+  end
 end
